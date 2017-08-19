19237598509a67769188ea15b73637aa2196c242
patrycjakwiecien <patrycjaapril@gmail.com>
2017-08-19 10:39:14 +0200

    plusy

---
+++ js/datatypes.js
@@ -0,0 +1,39 @@
+'use strict'
+
+
+
+/* Typ liczbowy */
+
+var wyplata = 5000;
+var premia = 2500;
+var calkowitaWyplata = "Zarabiam: " + wyplata + premia;
+
+console.log(calkowitaWyplata);
+
+
+/* Typ lancuch znakow string */
+
+
+
+var wyplataStr = "5000";
+
+var premiaStr = "2500";
+
+var calkowitaWyplataStr = "Zarabiam: " + wyplataStr + " " + premiaStr;
+
+console.log(calkowitaWyplataStr);
+
+/* Typ logiczny boolean */
+
+var smog = true;
+console.log(smog);
+
+
+
+/* Typ specjalny undefined/null */
+
+var niezdefiniowanaZmienna;
+var nullowaZmienna = null;
+
+console.log(niezdefiniowanaZmienna);
+console.log(nullowaZmienna);
